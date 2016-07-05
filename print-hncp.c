8e2e81b9680834b3bf7d5921d18820e20ccb4757
Antonin Décimo <antonin.decimo@gmail.com>
2016-07-05 12:15:31 +0200

    TLVs names

--- print-hncp.c
+++ print-hncp.c
@@ -19,6 +19,20 @@ static const char tstr[] = "[|hncp]";
 #define DNCP_KEEP_ALIVE_INTERVAL 9
 #define DNCP_TRUST_VERDICT 10
 
+/*
+static const struct tok dncp_type_values[] = {
+    { DNCP_REQUEST_NETWORK_STATE,	"Request network state" },
+    { DNCP_REQUEST_NODE_STATE,		"Request node state" },
+    { DNCP_NODE_ENDPOINT,		"Node endpoint" },
+    { DNCP_NETWORK_STATE,		"Network state" },
+    { DNCP_NETWORK_NODE_STATE,		"Network node state" },
+    { DNCP_PEER,		"Peer" },
+    { DNCP_KEEP_ALIVE_INTERVAL,		"Keep-alive interval" },
+    { DNCP_TRUST_VERDICT,		"Trust-Verdict" },
+    { 0, NULL}
+};
+*/
+
 #define HNCP_VERSION 32
 #define HNCP_EXTERNAL_CONNECTION 33
 #define HNCP_DELEGATED_PREFIX 34
@@ -31,6 +45,24 @@ static const char tstr[] = "[|hncp]";
 #define HNCP_DOMAIN_NAME 40
 #define HNCP_NODE_NAME 41
 #define HNCP_MANAGED_PSK 42
+
+/*
+static const struct tok hncp_type_values[] = {
+    { HNCP_VERSION,		"HNCP-Version" },
+    { HNCP_EXTERNAL_CONNECTION,		"External-Connection" },
+    { HNCP_DELEGATED_PREFIX,		"Delegated-Prefix" },
+    { HNCP_PREFIX_POLICY,		"Prefix-Policy" },
+    { HNCP_DHCPV6_DATA,		"DHCPv6-Data" },
+    { HNCP_DHCPV4_DATA,		"DHCPv4-Data" },
+    { HNCP_ASSIGNED_PREFIX,	"Assigned-Prefix" },
+    { HNCP_NODE_ADDRESS,		"Node-Address" },
+    { HNCP_DNS_DELEGATED_ZONE,		"DNS-Delegated-Zone" },
+    { HNCP_DOMAIN_NAME,		"Domain-Name" },
+    { HNCP_NODE_NAME,		"Node-Name" },
+    { HNCP_MANAGED_PSK,		"Managed-PSK" },
+    { 0, NULL}
+};
+*/
 
 void
 hncp_print(netdissect_options *ndo,
@@ -45,6 +77,14 @@ hncp_print(netdissect_options *ndo,
         const u_short type = EXTRACT_16BITS(tlv);
         const u_short len = EXTRACT_16BITS(tlv + 2);
         ND_TCHECK2(*tlv, 4 + len);
+
+        /*
+        if (ndo->ndo_vflag < 1) {
+            ND_PRINT((ndo, ", %s", tok2str(dncp_type_values, "unknown", type)));
+            i += len + 4;
+            continue;
+        }
+        */
 
         switch (type) {
         case DNCP_REQUEST_NETWORK_STATE: {
