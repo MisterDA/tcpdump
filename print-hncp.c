e08c93b388124ead55fb603268ae50e6106cd0c8
Antonin Décimo <antonin.decimo@gmail.com>
2016-07-05 10:46:01 +0200

    added TLVs to switch

--- print-hncp.c
+++ print-hncp.c
@@ -4,65 +4,144 @@
 
 #include <netdissect-stdinc.h>
 
-#include <stdio.h>
-#include <string.h>
-
 #include "netdissect.h"
-#include "addrtoname.h"
 #include "extract.h"
 
 static const char tstr[] = "[|hncp]";
 
 /* TLVs */
+#define DNCP_REQUEST_NETWORK_STATE 1
+#define DNCP_REQUEST_NODE_STATE 2
+#define DNCP_NODE_ENDPOINT 3
+#define DNCP_NETWORK_STATE 4
+#define DNCP_NETWORK_NODE_STATE 5
+#define DNCP_PEER 8
+#define DNCP_KEEP_ALIVE_INTERVAL 9
+#define DNCP_TRUST_VERDICT 10
+
 #define HNCP_VERSION 32
+#define HNCP_EXTERNAL_CONNECTION 33
+#define HNCP_DELEGATED_PREFIX 34
+#define HNCP_PREFIX_POLICY 43
+#define HNCP_DHCPV6_DATA 37
+#define HNCP_DHCPV4_DATA 38
+#define HNCP_ASSIGNED_PREFIX 35
+#define HNCP_NODE_ADDRESS 36
+#define HNCP_DNS_DELEGATED_ZONE 39
+#define HNCP_DOMAIN_NAME 40
+#define HNCP_NODE_NAME 41
+#define HNCP_MANAGED_PSK 42
 
 void
 hncp_print(netdissect_options *ndo,
-            const u_char *cp, u_int length)
+           const u_char *cp, u_int length)
 {
     ND_PRINT((ndo, "hncp"));
-    
+
     u_int i = 0;
-    
-    //ND_PRINT((ndo, " (%d)", length));
-    
-    while(i<length) {
+    while(i < length) {
         const u_char *tlv = cp + i;
-        
         ND_TCHECK2(*tlv, 4);
-        
         const u_short type = EXTRACT_16BITS(tlv);
-        const u_short len = EXTRACT_16BITS(tlv+2);
-        ND_TCHECK2(*tlv, 4+len);
-        
+        const u_short len = EXTRACT_16BITS(tlv + 2);
+        ND_TCHECK2(*tlv, 4 + len);
+
         switch (type) {
-            
-            case HNCP_VERSION: {
-                // do stuff
-            }
-                break;
-            
+        case DNCP_REQUEST_NETWORK_STATE: {
         }
-        
-        
-        
-        
-        
-        
-        
-        
+            break;
+
+        case DNCP_REQUEST_NODE_STATE: {
+        }
+            break;
+
+        case DNCP_NODE_ENDPOINT: {
+        }
+            break;
+
+        case DNCP_NETWORK_STATE: {
+        }
+            break;
+
+        case DNCP_NETWORK_NODE_STATE: {
+        }
+            break;
+
+        case DNCP_PEER: {
+        }
+            break;
+
+        case DNCP_KEEP_ALIVE_INTERVAL: {
+        }
+            break;
+
+        case DNCP_TRUST_VERDICT: {
+        }
+            break;
+
+        case HNCP_VERSION: {
+        }
+            break;
+
+        case HNCP_EXTERNAL_CONNECTION: {
+        }
+            break;
+
+        case HNCP_DELEGATED_PREFIX: {
+        }
+            break;
+
+        case HNCP_PREFIX_POLICY: {
+        }
+            break;
+
+        case HNCP_DHCPV6_DATA: {
+        }
+            break;
+
+        case HNCP_DHCPV4_DATA: {
+        }
+            break;
+
+        case HNCP_ASSIGNED_PREFIX: {
+        }
+            break;
+
+        case HNCP_NODE_ADDRESS: {
+        }
+            break;
+
+        case HNCP_DNS_DELEGATED_ZONE: {
+        }
+            break;
+
+        case HNCP_DOMAIN_NAME: {
+        }
+            break;
+
+        case HNCP_NODE_NAME: {
+        }
+            break;
+
+        case HNCP_MANAGED_PSK: {
+        }
+            break;
+
+        default:
+            ND_PRINT((ndo, "\n\tUnknown message type %d", type));
+        }
+
         /*
         ND_PRINT((ndo, " %d (%d)", type, len));
         for (int a=0; a<len; a++) {
             ND_PRINT((ndo, " %02hhX", *(tlv+4+a) ));
         }
         //*/
-        
-        i += 4+len;
+        i += len + 4;
     }
     return;
-    
-    trunc:
-       ND_PRINT((ndo, " %s", tstr));
-       return;
+
+ trunc:
+    ND_PRINT((ndo, " %s", tstr));
+    return;
 }
